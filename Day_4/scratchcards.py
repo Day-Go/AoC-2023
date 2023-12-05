c832b5d5e6c76e91b028d3af78bc4465f7d8fb4a
Day-Go <lucarusso.521@gmail.com>
2023-12-05 21:44:38 +1100

    day2 pt2

--- Day_4/scratchcards.py
+++ Day_4/scratchcards.py
@@ -1,23 +1,28 @@
 with open('input.txt', 'r') as f:
     lines = f.readlines()
 
+cards = [1] * len(lines)
+
 # Part 1
-sum = 0
-for line in lines:
-    line = line[10:]
+s = 0
+for i, line in enumerate(lines):
+    line = ''.join(line.split(':')[1:])
     winning_numbers = line.split('|')[0].split()
     my_numbers = line.split('|')[1].split()
 
     game_score = 0
     for num in my_numbers:
         if num in winning_numbers:
-            if game_score == 0:
-                game_score += 1
-            else:
-                game_score *= 2
+            game_score += 1
 
-    sum += game_score
+    r = min(i+game_score+1, len(cards))
+    for idx in range(i+1, r) :
+        cards[idx] += cards[i]
 
-print(sum)
+
+    s += game_score
+
+print(s)
+print(sum(cards))
 
 
